a331e092a9e751a09f52dacf63127e809d01dadb
Rebecca Wollard <rbwollard@gmail.com>
2022-12-22 13:32:31 -0700

    commit tree 6cf6aa80e075009daded199b03026418c4499e6b

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers (drivers) {
+    return drivers.slice(0, 2);
+}
+
+function returnLastTwoDrivers (drivers) {
+    return drivers.slice(drivers.length - 2, drivers.length);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(fareNumber) {
+    return function multiply(fareNumber) {
+        return fareNumber * fareNumber;
+    }
+}
+
+function fareDoubler(num) { 
+    let num2 = createFareMultiplier()
+    return function (num2) {
+        return num * num2 
+    }
+}
+
+function selectDifferentDrivers(drivers, driversArray) {
+    if (driversArray === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers()
+    } else {
+        return returnLastTwoDrivers()
+    }
+}
+
